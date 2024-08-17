4bd40ce6752f8e189f7fa940b94d5f0e5b3336d8
jwdavis1989 <jwdavis8589@gmail.com>
2024-08-17 18:23:36 -0500

    Added Car Shrink/Enlarge Mechanic

--- Assets/Scripts/CarGrowthController.cs
+++ Assets/Scripts/CarGrowthController.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class CarGrowthController : MonoBehaviour
 {
     [Header("Sizes")]
-    public float[] sizeArray = {0.5f, 1f, 2f};
+    public float[] sizeArray = { 0.5f, 1f, 2f };
+
+    [Header("Unity Set-up")]
+    //Time, in seconds, for size changes to take place
+    public float sizeChangeTimeInSeconds = 0.5f;
     private int currentScaleIndex = 1;
+    Rigidbody carRigidBody;
+    public float jumpForce = 10;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        InitializeScale();
+        carRigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -20,21 +27,46 @@ public class CarGrowthController : MonoBehaviour
         GetInputs();
     }
 
-    void GetInputs() {
+    void GetInputs()
+    {
         //Shrink
-        if (Input.GetKeyDown("q") && currentScaleIndex > 0) {
+        if (Input.GetKeyDown("q") && currentScaleIndex > 0)
+        {
             currentScaleIndex--;
-            updateScale(sizeArray[currentScaleIndex]);
+            Bounce();
+            UpdateScale(sizeArray[currentScaleIndex]);
         }
 
         //Grow
-        if (Input.GetKeyDown("e") && currentScaleIndex < 2) {
+        if (Input.GetKeyDown("e") && currentScaleIndex < 2)
+        {
             currentScaleIndex++;
-            updateScale(sizeArray[currentScaleIndex]);
+            Bounce();
+            UpdateScale(sizeArray[currentScaleIndex]);
         }
+
     }
 
-    void updateScale(float size) {
+    void InitializeScale()
+    {
+        transform.localScale = new Vector3(sizeArray[currentScaleIndex], sizeArray[currentScaleIndex], sizeArray[currentScaleIndex]);
+    }
+
+    void UpdateScale(float size)
+    {
         transform.localScale = new Vector3(size, size, size);
+    }
+
+    void UpdateScaleOverTime(float size)
+    {
+        Vector3 newScale = new Vector3(size, size, size);
+
+    }
+
+    void Bounce() {
+        carRigidBody.AddForce(transform.up * jumpForce * 1000 * (sizeArray[0]/sizeArray[2]), ForceMode.Impulse);
+        carRigidBody.mass = sizeArray[currentScaleIndex] * 950;
+        //Vector3 newVector3 = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+        //carRigidBody.MovePosition(newVector3);
     }
 }
