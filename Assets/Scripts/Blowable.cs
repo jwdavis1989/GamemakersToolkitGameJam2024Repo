1434fcf28597dc6e12ac9191ecde00a056f0638f
Alec Straw <47575949+akstraw@users.noreply.github.com>
2024-08-18 16:31:35 -0500

    terrain fixes, grass, road improvements, fan blows, boxes box, cheese

---
+++ Assets/Scripts/Blowable.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blowable : MonoBehaviour
+{
+    public bool inWindZone = false;
+    public GameObject windZone;
+
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        if (inWindZone)
+        {
+            WindBlowArea wind = windZone.GetComponent<WindBlowArea>();
+            rb.AddForce(wind.direction * wind.strength);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("windArea"))
+        {
+            windZone = other.gameObject;
+            inWindZone = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("windArea"))
+        {
+            inWindZone = false;
+        }
+    }
+}
